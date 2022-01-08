8cdd381e80e33642fa7e4ad26a5001418f7f248b
Dmitriy Tomilov <tomilov.d.a@mail.ru>
2022-01-08 13:04:37 +0400

    add versions, skip optimize

--- src/main/kotlin/ChromaticIndexGraphTask.kt
+++ src/main/kotlin/ChromaticIndexGraphTask.kt
@@ -5,10 +5,12 @@ import ru.dmitriyt.dcs.core.GraphInvariant
  */
 class ChromaticIndexGraphTask : GraphInvariant {
 
+    override val version = 1
+
     override fun solve(graph6: String): Int {
         val graph = Graph.fromGraph6(graph6)
         val (delta, maxDegVertex) = graph.maxDeg()
-        prepareGraphColoring(graph, maxDegVertex)
+//        prepareGraphColoring(graph, maxDegVertex)
         if (isDeltaColored(delta, 0, graph.edges.filter { !it.hasColor() }, graph)) {
             return delta
         }
